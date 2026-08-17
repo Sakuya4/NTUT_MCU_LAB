4ac0ab246083fc835700e01deb8299e6ced264b9
Sakuya4 <janalexei88@gmail.com>
2026-08-17 14:46:24 +0800

    LAB1-2

--- Core/Src/lab01_gpio.c
+++ Core/Src/lab01_gpio.c
@@ -9,3 +9,17 @@ void LAB1_1(void)
 	HAL_Delay(500);
   }
 }
+
+void LAB1_2(void)
+{
+  while(1)
+  {
+	  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+	  HAL_Delay(500);
+
+	  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+	  HAL_Delay(500);
+  }
+}
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -87,7 +87,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  LAB1_1();
+  LAB1_2();
   /* USER CODE END 2 */
 
   /* Infinite loop */
